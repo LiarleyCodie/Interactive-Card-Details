f9bbbec5ce3e9796a7108ce9ddd6d70eded43ed3
LiarleyCodie <liarley.personal@gmail.com>
2023-03-01 19:29:49 -0300

    stage: :dizzy: HTML and CSS almost completely written. All breakpoints defined

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const submitButtonEl = document.querySelector("#submitButton")
+// Header Cards
+// Card frontside
+const numberReferenceEl = document.querySelector("#numberReference")
+const nameReferenceEl = document.querySelector("#nameReference")
+const expDateReferenceEl = document.querySelector("#expDateReference")
 
-submitButtonEl.addEventListener("click", ev => ev.preventDefault())
+// Card backside
+const cvcReferenceEl = document.querySelector("#cvcReference")
+
+// Form Inputs
+const nameInputEl = document.querySelector("#cardName")
+const numberInputEl = document.querySelector("#cardNumber")
+const monthInputEl = document.querySelector("#expirationMonth")
+const yearInputEl = document.querySelector("#expirationYear")
+const cvcInputEl = document.querySelector("#cvcCode")
+const confirmButtonEl = document.querySelector("#confirmButton")
+
+confirmButtonEl.onclick = ev => ev.preventDefault()
