26304b128076c21077f5d313736e1bc97ebb7073
LiarleyCodie <liarley.personal@gmail.com>
2023-03-15 01:41:56 -0300

    stage: :tada: Inputs limited by number of digits. Auto spacing on card number input and cvc, mm and yy inputs limited by maximum number

---
+++ README.md
@@ -0,0 +1,4 @@
+CARDHOLDER NAME
+- Quando um texto for digitado, deve ser verificado se o texto digitado contém apenas letras (sem acentos) e espaços.
+- Se qualquer caractére for um caractére não permitida. A classe `error` deve ser adicionada
+- Se o input estiver vazil, o texto padrão do `nome de referência do cartão` será a propriedade `defaultPlaceholder`
--- index.js
+++ index.js
@@ -1,18 +1,109 @@
-// Header Cards
-// Card frontside
-const numberReferenceEl = document.querySelector("#numberReference")
-const nameReferenceEl = document.querySelector("#nameReference")
-const expDateReferenceEl = document.querySelector("#expDateReference")
+class Input {
+	constructor(element, length, type) {
+		this.element = document.querySelector(`#${element}`)
+		this.length = length
+		this.type = type
+	}
+	init() {
+		this.element.addEventListener("input", () => this.parseChars())
+	}
+	parseChars() {
+		if (this.element.value.length > this.length)
+			this.element.value = this.element.value.slice(0, this.length)
 
-// Card backside
-const cvcReferenceEl = document.querySelector("#cvcReference")
+		const currentValue = this.element.value
+		const newValue = currentValue.replace(/[^0-9]/g, "")
+		this.element.value = newValue
+		if (this.type === "month") {
+			this.limitMonth()
+		}
+	}
+	limitMonth() {
+		if (parseInt(this.element.value) > 12) {
+			this.element.value = 12
+		} else if (parseInt(this.element.value) <= 0) {
+			this.element.value = 1
+		}
+	}
+}
 
-// Form Inputs
-const nameInputEl = document.querySelector("#cardName")
-const numberInputEl = document.querySelector("#cardNumber")
-const monthInputEl = document.querySelector("#expirationMonth")
-const yearInputEl = document.querySelector("#expirationYear")
-const cvcInputEl = document.querySelector("#cvcCode")
-const confirmButtonEl = document.querySelector("#confirmButton")
+var existInvalidEntry = false
 
-confirmButtonEl.onclick = ev => ev.preventDefault()
+onload = () => {
+	cardNameInput.init()
+	cardNumberInput.init()
+	cvcInput.init()
+	monthInput.init()
+	yearInput.init()
+}
+
+const cardNameInput = {
+	element: document.querySelector("#cardNameInput"),
+	wrapper: document.querySelector("#nameInputWrapper"),
+	chars: "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	init: function() {
+		this.element.addEventListener("input", () => {
+			this.updateErrorElement(this.checkEntry())
+		})
+	},
+	checkEntry: function() {
+		const inputValue = this.element.value
+		const validEntry = inputValue.split("").every(char => this.chars.includes(char))
+		if (validEntry) {
+			existInvalidEntry = false
+			return true
+		}
+		else {
+			existInvalidEntry = true
+			return false
+		}
+	},
+	updateErrorElement: function(state) {
+		if (!state)
+			this.wrapper.classList.add("error")
+		else
+			this.wrapper.classList.remove("error")
+	}
+}
+const cardNumberInput = {
+	element: document.querySelector("#cardNumberInput"),
+	wrapper: document.querySelector("#cardNumberWrapper"),
+	init: function() {
+		this.element.addEventListener("input", () => {
+			if (this.element.value[0] == " ") {
+				this.element.value = this.element.value.trimStart()
+			} else {
+				this.autoAddWhitespaces()
+				if (this.element.value.length === 1 && this.element.value[0] == " ") {
+					this.element.value = ""
+				}
+			}
+
+			this.parseChars()
+		})
+	},
+	updateErrorElement: function(state) {
+		if (!state)
+			this.wrapper.classList.add("error")
+		else
+			this.wrapper.classList.remove("error")
+	},
+	parseChars: function() {
+		const currentValue = this.element.value
+		const newValue = currentValue.replace(/[^0-9 ]/g, "")
+		this.element.value = newValue
+	},
+	autoAddWhitespaces: function() {
+		const currentValue = this.element.value
+		const valueWithoutSpaces = currentValue.replace(/\s+/g, "")
+		if (valueWithoutSpaces.length % 4 === 0 && currentValue.length < 19)
+			this.element.value = currentValue + " "
+	}
+}
+const cardNameReference = {
+	element: document.querySelector("#nameReference")
+}
+
+const cvcInput = new Input("cvcCodeInput", 3)
+const monthInput = new Input("expirationMonthInput", 2, "month")
+const yearInput = new Input("expirationYearInput", 2)
